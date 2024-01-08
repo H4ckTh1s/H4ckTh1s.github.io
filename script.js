e08b5c4d3f7fad962260a6405e77f76ce2603069
H4ckTh1s <155689858+H4ckTh1s@users.noreply.github.com>
2024-01-08 01:15:02 +0100

    Final Update script.js
    Cambio por completo del contenido .js, resolviendo los problemas anteriores.

--- script.js
+++ script.js
@@ -1,124 +1,112 @@
+function compressIPv6Array(ipv6Array) {
+    let compressedIPv6 = ipv6Array
+    .map(part => (part.replace(/^0+/, '') || '0')) 
+    .join(':'); 
 
+    // Busca y reemplaza bloques consecutivos de ceros solo si hay más de un bloque
+    const consecutiveZerosRegex = /((:|^)0(:|$)){2,}/g;
+    compressedIPv6 = compressedIPv6.replace(consecutiveZerosRegex, '::');
 
-// Mostrar el resultado
-//console.log(resultado);
+    return compressedIPv6;
+}
 
-function convertirIpv6(cadena) {
-    
-     // Dividir la cadena por puntos y convertir cada fragmento a hexadecimal
-  let hexArray = cadena.split(':').map(partes => {
-    // Convertir el segmento a decimal y luego a hexadecimal
-    const decimalValue = parseInt(partes, 10);
-    const hexadecimalValue = decimalValue.toString(16);
-    return hexadecimalValue;
-  });
-  //console.log(hexArray);
-  let arrayFinal = [];
+function dividirArrayEnDos(array) {
+    return array.map(elemento => [elemento.slice(0, 2), elemento.slice(2)]);
+}
 
-  for(let i = 1; i <= hexArray.length;i++){
-    if(i%2 == 1){
-        if(hexArray[i-1] == 0){
-            let contadorCeros = 0;
-            for(let j=i; j <= hexArray.length; j++){
-                if(hexArray[j-1] == 0){
+function resultadoFinal(texto){
+    let textoFinal = "";
+    let array = texto.split(":");
+    console.log(array)
+    for(let i = 1; i <= array.length; i++){
+        let contadorCeros = 0;
+        
+        if(array[i-1] == "0"){
+
+            for(let j=i; j <= array.length; j++){
+                if(array[j-1] == 0){
                     contadorCeros++;
                 }else{
                     break;
                 }
             }
 
-            if (contadorCeros >= 4){
+            if(contadorCeros > 1){
 
-                if(contadorCeros %2 == 0){
-                    i=i+contadorCeros;
-                    i--;
-                }else{
-                    let calc = contadorCeros - 1;
-                    i=i+calc;
-                    i--;
-                }
-            
-                arrayFinal.push(":");
-
-            }else if(contadorCeros == 1){
-
-            }else{
-                arrayFinal.push("0");
-                arrayFinal.push("0");
-                i=i+2;
+                textoFinal += ":";
+                i = i+contadorCeros;
                 i--;
-            } 
 
-        }else{
-            arrayFinal.push(hexArray[i-1]);
-        }
-
-
-    }else{
-        if(hexArray[i-1] == 0){
-            arrayFinal.push("00");
-        }else{
-  
-            arrayFinal.push(hexArray[i-1]);
-        }
-    }
-
-  }
-
-  let cadenaFinal = "";
-  console.log(hexArray);
-  console.log(arrayFinal);
-  for(i=1; i <= arrayFinal.length; i++){
-
-    if(i%2 == 0){
-        console.log("Numero: " + arrayFinal[i-1] + " longitud: " + arrayFinal[i-1].length)
-        if(arrayFinal[i-1].length == 1){
-
-            if(arrayFinal[i-1] == ":"){
-                cadenaFinal += arrayFinal[i-1];
             }else{
-                cadenaFinal += "0";
-                cadenaFinal += arrayFinal[i-1];
+                textoFinal += array[i-1];
+                textoFinal += ":";
             }
+
         }else{
-            cadenaFinal += arrayFinal[i-1];
+            textoFinal += array[i-1];
+            textoFinal += ":";
         }
-        
-        cadenaFinal += ":";
-
-    }else{
-        cadenaFinal += arrayFinal[i-1];
     }
-
-    if(arrayFinal[i-1] == "0"){
-        cadenaFinal += ":";
-        i++;
-    }
-    
-
-  }
-
-  const result = cadenaFinal;
-
-  return result;
+    textoFinal = textoFinal.replace(/:$/, "");
+    return textoFinal;
 
 }
-  
+
 
 document.getElementById('botonProcesar').addEventListener('click', function() {
     const cadena = document.getElementById('inputTexto').value;
     const resultadoElemento = document.getElementById('resultado');
     
-    // Dividir la cadena en un array usando el punto como separador
     var elementos = cadena.split(".");
 
-    // Seleccionar los últimos 16 elementos
     var ultimos16 = elementos.slice(-16);
 
-    // Unir los elementos con el separador punto
     var resultado = ultimos16.join(":");
-    var resultadoHexadecimal = convertirIpv6(resultado);
+     
+     let hexArray = resultado.split(':').map(partes => {
+        // Convertir el segmento a decimal y luego a hexadecimal
+        const decimalValue = parseInt(partes, 10);
+        const hexadecimalValue = decimalValue.toString(16);
+        return hexadecimalValue;
+      });
+      console.log(hexArray);
 
-    resultadoElemento.textContent = 'IPV6: ' + resultadoHexadecimal;
+      for (let i = 1; i <= hexArray.length; i++) {
+
+        if (i%2==0 && hexArray[i-1].length === 1 || i%2==1 && hexArray[i-1] == "0") {
+            hexArray[i-1] = "0" + hexArray[i-1];
+        }
+      }
+
+
+      let texto = ""
+      for(let i = 1; i <= hexArray.length; i++){
+        if(i%2==0){
+            
+            if(i == hexArray.length){
+                texto += hexArray[i-1]
+            }else{
+                texto += hexArray[i-1]
+                texto += ":"
+            }
+
+        }else{
+            texto += hexArray[i-1]
+        }
+      }
+      console.log(texto);
+      let arrayTexto = texto.split(":");
+      console.log(arrayTexto);
+
+      let arrayDividio = dividirArrayEnDos(arrayTexto);
+      console.log(arrayDividio);
+      
+      const compressedIPv6 = compressIPv6Array(arrayTexto);
+      const Ipv6Final = resultadoFinal(compressedIPv6);
+      console.log(compressedIPv6)
+
+
+    resultadoElemento.textContent = 'IPV6: ' + Ipv6Final;
 });
   
+
